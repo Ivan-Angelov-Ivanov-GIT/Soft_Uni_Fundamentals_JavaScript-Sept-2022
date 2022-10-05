c740cc1696704dd0901365265bb06ab86d111ffa
Ivan-Angelov-Ivanov-GIT <114222368+Ivan-Angelov-Ivanov-GIT@users.noreply.github.com>
2022-10-05 18:15:42 +0300

    Update

---
+++ Arrays/arraysplay.js
@@ -0,0 +1,12 @@
+// Решавам 8. Condense Array to Number
+
+function solve(nums){
+
+    let condensed = [];
+
+    for (element of nums){
+        console.log(element);
+    }
+
+
+}solve([2,10,3]);

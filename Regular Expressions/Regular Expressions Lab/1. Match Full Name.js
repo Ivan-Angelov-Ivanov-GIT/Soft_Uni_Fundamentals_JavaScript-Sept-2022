d06704f4cca00bda0766f4195df574bbeaad4ab5
Ivan Ivanov <114222368+Ivan-Angelov-Ivanov-GIT@users.noreply.github.com>
2022-11-19 23:14:01 +0200

    updated

--- Regular Expressions/Regular Expressions Lab/1. Match Full Name.js
+++ Regular Expressions/Regular Expressions Lab/1. Match Full Name.js
@@ -1,1 +1,9 @@
--1 : 25.7
+function matchNames (text){
+let pattern = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/g;
+
+let names = text.match(pattern);
+
+console.log(names.join(' '));
+
+
+}matchNames('ivan ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Ivan IvAnov, Ivan Ivanov');

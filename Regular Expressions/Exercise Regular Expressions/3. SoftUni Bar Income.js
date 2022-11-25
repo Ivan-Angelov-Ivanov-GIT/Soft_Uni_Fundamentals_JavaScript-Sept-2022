872dffc01db8c3122ff622e2327f1fbc4caaddb3
Ivan Ivanov <114222368+Ivan-Angelov-Ivanov-GIT@users.noreply.github.com>
2022-11-25 09:46:17 +0200

    update

--- Regular Expressions/Exercise Regular Expressions/3. SoftUni Bar Income.js
+++ Regular Expressions/Exercise Regular Expressions/3. SoftUni Bar Income.js
@@ -1,5 +1,32 @@
 function barIncome(customerArray) {
-    const pattern = /%(?<customer>[A-Za-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*|(?<count>[0-9]+)|[^|$%.]*(?<price>[0-9|\.]+)\$/g
 
+    const pattern = /%(?<customer>[A-Za-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>[0-9]+)\|[^|$%.]*(?<price>[0-9|\.]+)\$/g
+
+    let totalIncome = 0;
+    let command = customerArray.shift();
+
+    while (command !== 'end of shift') {
+        let currentData = pattern.exec(command);
+
+        if (currentData){
+            const currentCustomer = currentData.groups['customer'];
+            const currentProduct = currentData.groups['product'];
+            const currentCount = Number(currentData.groups['count']);
+            const currentPrice = Number(currentData.groups['price']);
+
+            const totalProductPrice = currentCount * currentPrice;
+
+            console.log(`${currentCustomer}: ${currentProduct} - ${totalProductPrice}`);
+
+            totalIncome += totalProductPrice;
+
+           
+        } else {
+            command = customerArray.shift();
+            // currentData = pattern.exec(command);
+        }
+       
+    }
+    console.log(`Total income: ${totalIncome.toFixed(2)}`)
 }
 barIncome(['%George%<Croissant>|2|10.3$', '%Peter%<Gum>|1|1.3$', '%Maria%<Cola>|1|2.4$', 'end of shift']);

7d20556566bfbe5e46bf71497d353323d2b7f0b6
Ivan-Angelov-Ivanov-GIT <114222368+Ivan-Angelov-Ivanov-GIT@users.noreply.github.com>
2022-11-25 07:06:01 +0200

    upload

--- Regular Expressions/Exercise Regular Expressions/3. SoftUni Bar Income.js
+++ Regular Expressions/Exercise Regular Expressions/3. SoftUni Bar Income.js
@@ -1,1 +1,5 @@
-function barIncome(customerArray) {}barIncome(['%George%<Croissant>|2|10.3$', '%Peter%<Gum>|1|1.3$', '%Maria%<Cola>|1|2.4$', 'end of shift']);
+function barIncome(customerArray) {
+    const pattern = /%(?<customer>[A-Za-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*|(?<count>[0-9]+)|[^|$%.]*(?<price>[0-9|\.]+)\$/g
+
+}
+barIncome(['%George%<Croissant>|2|10.3$', '%Peter%<Gum>|1|1.3$', '%Maria%<Cola>|1|2.4$', 'end of shift']);
